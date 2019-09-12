c0ddfdd6f839183b8095517d20348d8a4740f181
yoktavian <yoktavian17@gmail.com>
2019-09-12 21:30:27 +0700

    init git

---
+++ app/src/main/java/com/intech/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.intech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
